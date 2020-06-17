733f4dbb4b3c9979d2bba93a3e9568bb0e728a8f
Rajdeep Sengupta <senguptarajdeep21@gmail.com>
2020-06-17 19:26:17 +0530

    added move to top btn

--- index.js
+++ index.js
@@ -250,3 +250,20 @@ function fetchNews(){
             })
             .catch(error=>console.log("error"))
 }
+
+// MOVE TO TOP BTN
+var mybutton = document.getElementById("topBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
