2e1092ee5f3038f9951c80709f051ae732192d1b
Rajdeep Sengupta <senguptarajdeep21@gmail.com>
2020-06-18 11:08:04 +0530

    added dark mode

--- index.js
+++ index.js
@@ -267,3 +267,9 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// DARK MODE
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
